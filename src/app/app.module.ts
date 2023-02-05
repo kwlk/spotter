973d9ef156a65b855c163b4de2fb3fceef523dd9
filip6464 <filip-zyga@wp.pl>
2023-02-05 13:44:38 +0100

    updates after review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {CalendarEventsComponent} from './components/calendar-events/calendar-events.component';
 import {LoginComponent} from './components/login/login.component';
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
@@ -25,7 +24,6 @@ import { EventComponent } from './components/event/event.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarEventsComponent,
     LoginComponent,
     HeaderComponent,
     MapComponent,
